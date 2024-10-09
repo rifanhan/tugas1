9dd51eeb5185c6d8da73a5c8344674c8cf902e6e
rifanhan <rifanhanafi365@gmail.com>
2024-10-09 20:22:52 +0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,47 @@
+function portofolio() {
+	document.getElementById('keren').innerHTML = `
+		<div class="halo-keren">
+			<h3>Tentang Saya</h3>
+			
+			<li>Saya mahasiswa di universitas islam negri sultan syarif kasim </li>
+		
+	`;
+}
+
+function cv() {
+	document.getElementById('keren').innerHTML = `
+		<div class="halo-keren">
+			<h3>Curriculum Vitae</h3>
+			<table border="1">
+				<tr>
+					<td>Tempat, Tanggal Lahir</td>
+					<td>Perawang, 2 april 1999</td>
+				</tr>
+				<tr>
+					<td>Status Perkawinan</td>
+					<td>Belum Menikah</td>
+				</tr>
+				<tr>
+					<td>Jenis Kelamin</td>
+					<td>Laki-Laki</td>
+				</tr>
+				<tr>
+					<td>Tinggi Badan</td>
+					<td>170cm</td>
+				</tr>
+				<tr>
+					<td>Agama</td>
+					<td>Islam</td>
+				</tr>
+				<tr>
+					<td>Alamat Asal</td>
+					<td> Perawang Siak </td>
+				</tr>
+				<tr>
+					<td>Alamat Tinggal</td>
+					<td>Perumahan Vila Perawang Indah</td>
+				</tr>
+			</table>
+		</div>
+	`
+}
